b53b57c14c8391150e7b4348143c08c086031be6
Graham Bell <graham.s.bell@gmail.com>
2016-08-20 10:25:03 -1000

    Initial version of OrderBlocks extension

---
+++ lib/jinja2_orderblocks/orderblocks.py
@@ -0,0 +1,67 @@
+# OrderBlocks extension for Jinja2
+# Copyright (C) 2016 Graham Bell
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from jinja2 import ext, nodes
+
+
+class OrderBlocks(ext.Extension):
+    """
+    Extension for Jinja2 to reorder blocks.
+
+    This adds a tag `{% orderblocks %} ... {% endorderblocks %}`
+    which can be used to change the order of the blocks it contains.
+    It takes one argument which should be a list (or other iterable)
+    of block names in the order in which they should be displayed.
+    Block names can be omitted from the list to prevent them from
+    being shown at all.  Nodes other than blocks inside the `orderblocks`
+    section are ignored.
+
+    >>> from jinja2 import Environment
+    >>> env = Environment(extensions=[
+    ...     'jinja2_orderblocks.OrderBlocks'])
+    >>> template = env.from_string(
+    ...     '{% orderblocks block_order %}'
+    ...     '{% block one %}1{% endblock %}'
+    ...     '{% block two %}2{% endblock %}'
+    ...     '{% endorderblocks %}')
+    >>> template.render(block_order=['two', 'one'])
+    '21'
+    """
+
+    tags = set(['orderblocks'])
+
+    def parse(self, parser):
+        lineno = next(parser.stream).lineno
+
+        # Get the block selection from the tag argument.
+        block_selection = parser.parse_expression()
+
+        # Make "Name" node for "For" node target.
+        block_name = nodes.Name('_ext_ob_blknm', 'store')
+
+        # For each block, add an "If" node checking if it matches the target.
+        blocks = []
+        for node in parser.parse_statements(['name:endorderblocks'],
+                                            drop_needle=True):
+            if isinstance(node, nodes.Block):
+                blocks.append(nodes.If(
+                    nodes.Compare(
+                        block_name,
+                        [nodes.Operand('eq', nodes.Const(node.name))]),
+                    [node], []))
+
+        # Make a "For" node iterating over the given block selection.
+        return nodes.For(block_name, block_selection, blocks, [], None, False)
---
+++ test/test_orderblocks.py
@@ -0,0 +1,48 @@
+# OrderBlocks extension for Jinja2 -- Test script
+# Copyright (C) 2016 Graham Bell
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from unittest import TestCase
+
+from jinja2 import Environment
+
+from jinja2_orderblocks import OrderBlocks
+
+
+class OrderBlockTestCase(TestCase):
+    def test_basic(self):
+        env = Environment(extensions=[OrderBlocks])
+
+        template = env.from_string(
+            'a'
+            '{% orderblocks blocks %}'
+            '{% block x %}b{% endblock %}'
+            '{% block y %}c{% endblock %}'
+            '{% block z %}d{% endblock %}'
+            '{% endorderblocks %}'
+            'e')
+
+        self.assertEqual(template.render(blocks=('x', 'y', 'z')), 'abcde')
+        self.assertEqual(template.render(blocks=('z', 'y', 'x')), 'adcbe')
+        self.assertEqual(template.render(blocks=('z', 'x', 'y')), 'adbce')
+        self.assertEqual(template.render(blocks=('x', 'z', 'y')), 'abdce')
+        self.assertEqual(template.render(blocks=('x', 'y')), 'abce')
+        self.assertEqual(template.render(blocks=('x', 'z')), 'abde')
+        self.assertEqual(template.render(blocks=('y', 'z')), 'acde')
+        self.assertEqual(template.render(blocks=('z', 'y')), 'adce')
+        self.assertEqual(template.render(blocks=('z', 'x')), 'adbe')
+        self.assertEqual(template.render(blocks=('x')), 'abe')
+        self.assertEqual(template.render(blocks=('y')), 'ace')
+        self.assertEqual(template.render(blocks=('z')), 'ade')
