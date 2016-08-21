9acdf3808339328794acad457614ae02712b5cc5
Graham Bell <graham.s.bell@gmail.com>
2016-08-20 14:10:06 -1000

    setup: Add setup script

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='jinja2_orderblocks',
+    version='0.0.0',
+    description='OrderBlocks Extension for Jinja2',
+    long_description=long_description,
+    author='Graham Bell',
+    author_email='graham.s.bell@gmail.com',
+    url='http://github.com/grahambell/jinja2-orderblocks',
+    package_dir={'': 'lib'},
+    packages=['jinja2_orderblocks'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
